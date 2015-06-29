2802cf6d7afdae002873909c4acbced09bb52278
Lawrence Campbell <lawrence.campbell@strath.ac.uk>
2015-06-29 01:20:11 +0100

    Added commented out temp routine for wiggler entry and exit sections in HwigglerVar.f90.
    -Lawrence

--- source/HwigglerVar.f90
+++ source/HwigglerVar.f90
@@ -3,6 +3,7 @@ module wigglerVar
 use globals
 use paratype
 use ParallelInfoType
+use lattice
 
 contains 
 
@@ -21,6 +22,33 @@ subroutine getAlpha(sZ)
   n2col = n2col0  + undgrad*(sz - sz0)
 
 end subroutine getAlpha
+
+
+
+! subroutine getAlpha_end(sZ)
+
+! ! Calculates linear taper based on
+! ! global variable undGrad
+
+!   real(kind=wp), intent(in) :: sZ
+
+! ! Variables n2col, c2col0, undgrad and sz0 are
+! ! global variables, and are defined elsewhere.
+
+!   real(kind=wp) :: Nwt
+
+!   Nwt = 2.0_wp
+
+!   if (endType == iFront) then
+!     n2col = n2col0 * sin((sz-sz0) / 8.0_wp / sRho_G / Nwt)**2.0_wp
+!   else if (endType == iBack)
+!     n2col = n2col0 * sin((sz-sz0) / 8.0_wp / sRho_G / Nwt)**2.0_wp
+!   end if
+
+
+! end subroutine getAlpha_end
+
+
 
 
 end module wigglerVar
