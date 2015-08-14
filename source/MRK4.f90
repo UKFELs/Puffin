480564d632ca18d0024ab5078c67be32f0264af5
Lawrence Campbell <lawrence.campbell@strath.ac.uk>
2015-08-14 02:42:14 +0100

    TEMP COMMIT - WILL NOT COMPILE
    -Lawrence

--- source/MRK4.f90
+++ source/MRK4.f90
@@ -53,7 +53,7 @@ SUBROUTINE rk4par(sA,A_local,x,h,recvs,displs,qD)
   REAL(KIND=WP)    :: h6, hh, xh
   !REAL(KIND=WP), DIMENSION(size(y)) :: dym, dyt, yt
 
-  REAL(KIND=WP), DIMENSION(iNumberElectrons_G) :: dxm, dxt, xt
+  REAL(KIND=WP), DIMENSION(iNumberElectrons_G) :: dxm, dxt, xt    ! *t is 'temp', for use in next rhs call...
   REAL(KIND=WP), DIMENSION(iNumberElectrons_G) :: dym, dyt, yt
   REAL(KIND=WP), DIMENSION(iNumberElectrons_G) :: dpxm, dpxt, pxt
   REAL(KIND=WP), DIMENSION(iNumberElectrons_G) :: dpym, dpyt, pyt
@@ -119,13 +119,22 @@ SUBROUTINE rk4par(sA,A_local,x,h,recvs,displs,qD)
 
   CALL derivs(x, &
        sA, &
-       dydx,&
+       sElX_G, sElY_G, sElZ2_G, sElPX_G, sElPY_G, sElPZ2_G, &
+       dxdx, dydx, dz2dx, dpxdx, dpydx, dpz2dx, &
        dAdx)
 
   ALLOCATE(dAm(2*local_rows),dAt(2*local_rows))
 
 !    Increment local electron and field values
-  yt = y + hh * dydx
+  
+  xt = sElX_G      +  hh*dxdx
+  yt = sElY_G      +  hh*dydx
+  z2t = sElZ2_G    +  hh*dz2dx
+  pxt = sElPX_G    +  hh*dpxdx
+  pyt = sElPY_G    +  hh*dpydx
+  pz2t = sElPZ2_G  +  hh*dpz2dx
+
+
   A_localt = A_local + hh * dAdx  	  
 
 !    Update large field array with new values 
@@ -136,13 +145,19 @@ SUBROUTINE rk4par(sA,A_local,x,h,recvs,displs,qD)
 
   CALL derivs(xh, &
        sA, &
-       yt, &
-       dyt,&
+       xt, yt, z2t, pxt, py2, pz2t, &
+       dxt, dyt, dz2t, dpxt, dpyt, dz2t, &
        dAt)
 
 !    Incrementing with newest derivative value...
 
-  yt = y + hh * dyt
+  xt = sElX_G      +  hh*dxt
+  yt = sElY_G      +  hh*dyt
+  z2t = sElZ2_G    +  hh*dz2t
+  pxt = sElPX_G    +  hh*dpxt
+  pyt = sElPY_G    +  hh*dpyt
+  pz2t = sElPZ2_G  +  hh*dpz2t
+
   A_localt = A_local + hh * dAt
 
 !    Update full field array
@@ -154,18 +169,30 @@ SUBROUTINE rk4par(sA,A_local,x,h,recvs,displs,qD)
 
   CALL derivs(xh, &
        sA, &
-       yt, &
-       dym,&
+       xt, yt, z2t, pxt, py2, pz2t, &
+       dxm, dym, dz2m, dpxm, dpym, dz2m, &
        dAm)
 
 !    Incrementing
 
-  yt = y + h * dym
+  xt = sElX_G      +  h*dxm
+  yt = sElY_G      +  h*dym
+  z2t = sElZ2_G    +  h*dz2m
+  pxt = sElPX_G    +  h*dpxm
+  pyt = sElPY_G    +  h*dpym
+  pz2t = sElPZ2_G  +  h*dpz2m
+
   A_localt=A_local + h * dAm
 
   CALL local2globalA(A_localt,sA,recvs,displs,tTransInfo_G%qOneD)
 
+  dxm = dxt + dxm
   dym = dyt + dym
+  dz2m = dz2t + dz2m
+  dpxm = dpxt + dpxm
+  dpym = dpyt + dpym
+  dpz2m = dpz2t + dpz2m
+
   dAm = dAt + dAm
 
 !    Fourth step       
@@ -176,13 +203,21 @@ SUBROUTINE rk4par(sA,A_local,x,h,recvs,displs,qD)
 
   CALL derivs(xh, &
        sA, &
-       yt, &
-       dyt,&
+       xt, yt, z2t, pxt, py2, pz2t, &
+       dxt, dyt, dz2t, dpxt, dpyt, dz2t, &
        dAt)  
 
 !    Accumulate increments with proper weights       
 
-  y = y + h6 * (dydx + dyt + 2.0_WP * dym)
+  sElX_G    = sElX_G   + h6 * ( dxdx   + dxt   + 2.0_WP * dxm  )
+  sElY_G    = sElY_G   + h6 * ( dydx   + dyt   + 2.0_WP * dym  )
+  sElZ2_G   = sElZ2_G  + h6 * ( dz2dx  + dz2t  + 2.0_WP * dz2m )
+  sElPX_G   = sElPX_G  + h6 * ( dpxdx  + dpxt  + 2.0_WP * dpxm )
+  sElPY_G   = sElPY_G  + h6 * ( dpydx  + dpyt  + 2.0_WP * dpym )
+  sElPZ2_G  = sElPZ2_G + h6 * ( dpz2dx + dpz2t + 2.0_WP * dpz2m)
+
+
+
   A_local = A_local + h6 * (dAdx + dAt + 2.0_WP * dAm)
 
   CALL local2globalA(A_local,sA,recvs,displs,tTransInfo_G%qOneD)
