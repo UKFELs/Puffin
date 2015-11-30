06eb8ec63e4940f2f7110299a792ed6fc3ea2d84
Lawrence Campbell <lawrence.campbell@strath.ac.uk>
2015-11-30 16:36:53 +0000

    TEMP COMMIT - MAY NOT COMPILE
    -Lawrence

--- source/MASPin.f90
+++ source/MASPin.f90
@@ -7,12 +7,17 @@ module MASPin
 
 
     integer(kind=ip) :: nMPs, nMPsLoc
+    integer(kind=ip) :: nBlanks_head
     integer :: fid
+
+    real(kind=wp) :: dV_bar
     
     real(kind=wp) :: dummy1, dummy2, dummy3
 
 
     fid = 132
+
+    dV_bar = 1.0_wp
 
     ! Split into local num per process
 
@@ -22,7 +27,9 @@ module MASPin
     call splitBeam(nMPs, dummy1, tProcInfo_G%size, tProcInfo_G%rank, &
                    nMPsLoc, dummy2, dummy3)
 
+    allocate(recvs_eb(tProcInfo_G%size), displs_eb(tProcInfo_G%size))
 
+    call getGathArrs(nMPsLoc,recvs_eb,displs_eb)
 
     ! Allocate local MP arrays
 
@@ -39,6 +46,8 @@ module MASPin
 
     ! read file
 
+    nBlanks_head = 1_ip   ! number of lines in header
+
     do ir = 0,tProcInfo_G%size-1
 
       if (ir == tProcInfo_G%rank) then
@@ -46,11 +55,13 @@ module MASPin
         OPEN(UNIT=fid,FILE=zFile,IOSTAT=ios,&
              ACTION='READ',POSITION='REWIND')   
   
+        nBlanks = displs_eb(ir) + nBlanks_head
+
         do ij = 1,nBlanks 
         	READ(UNIT=fid, FMT=*) 
         end do
 
-        do ij = 1,nMPsLoc
+        do ij = displs_eb(ir)+1, nMPsLoc + displs_eb(ir)
 
           read(UNIT=fid, FMT=*) sElX_G(ij), sElY_G(ij), &
                                 sElPX_G(ij), sElPY_G(ij), &
@@ -64,6 +75,9 @@ module MASPin
       
 
     end do
+
+
+    s_Normalised_chi_G = s_chi_bar_G / dV_bar
 
     ! exit
 
