f11e87773d901323b5cb4d1de9464588b0210a39
Jonny Smith <jonathan.smith@txcorp.co.uk>
2016-09-09 00:37:33 +0100

    Reads particles from h5 file - in series is still quick, compared with time spent in allreduce calculation around line 1000 of Jsetupcalcs.f90

--- source/H5in.f90
+++ source/H5in.f90
@@ -22,6 +22,7 @@ contains
     INTEGER(HID_T) :: dtype         !< So we can check we're reading in doubles
     INTEGER(HID_T) :: dclass         !< So we can check we're reading in doubles
     INTEGER(HID_T) :: filespace     !< Dataspace identifier in file
+    INTEGER(HID_T) :: memspace     !< Dataspace identifier in file
     INTEGER(HID_T) :: attr_id       !< Attribute identifier
     INTEGER(HID_T) :: aspace_id     !< Attribute Dataspace identifier
     INTEGER(HID_T) :: atype_id      !< Attribute Data type identifier
@@ -55,6 +56,9 @@ contains
     integer :: error ! Error flag
     character(LEN=40) :: errorstr !<String to write an error
     integer(kind=ip) :: nMPs
+    integer(kind=ip) :: nMPsLoc
+    integer(kind=ip) :: mpiinfo
+    mpiinfo=MPI_INFO_NULL
 
     filename = zfile ! unless this is naughty due to different length
     ! could filename need a trim
@@ -74,10 +78,12 @@ contains
       CALL h5tget_class_f (dtype, dclass, error)
       Print*,'hdf5_puff:readH5Beamfile(dataset opened in serial)'
       Print*,error
-      if (dclass==H5T_NATIVE_DOUBLE) then
-       print*,'data is double precision'
+!      if (dclass==H5T_NATIVE_DOUBLE) then
+      if (dclass==H5T_FLOAT_F) then
+       print*,'data is float'
       else
-      errorstr = trim("data is not double float")
+      errorstr = trim("data is no float")
+      print*,dclass
       goto 1000
       end if
       CALL h5Dget_space_f(dset_id,dspace_id,error)
@@ -85,27 +91,173 @@ contains
       Print*,error
       CALL h5Sget_simple_extent_ndims_f(dspace_id,rank,error)
       Print*,'hdf5_puff:readH5Beamfile(dataspace opened in serial)'
+      Print*,rank
       Print*,error
-      if (dclass==2) then
+      if (rank==2) then
        print*,'data rank is 2, which is good'
       else
-      errorstr = trim("data does not have rank 2")
+      errorstr = trim("data does not have rank 2, has rank")
       goto 1000
       end if
       CALL h5Sget_simple_extent_dims_f(dspace_id,dims,mdims,error)
-      Print*,'hdf5_puff:readH5Beamfile(dataspace opened in serial)'
-      Print*,error
-      if (dims(2)==7) then
+      Print*,'hdf5_puff:readH5Beamfile(dataspace getting dims)'
+      Print*,error ! rank on success = 2
+      print*,dims
+      if (dims(1)==7) then
        print*,'data has seven columns, which is good'
       else
       errorstr = trim("data does not have seven columns")
       goto 1000
       end if
+     nMPs=dims(2)
+     iGloNumElectrons_G = nMPs
      print*,"number of particles in file: "
-     print*, dims(1)
-   end if
+     print*, nMPs
+     call h5sclose_f(dspace_id,error) !dspace_id
+     print*,error
+     print*,"h5s closed"
+     call h5dclose_f(dset_id,error)
+     print*,error
+     print*,"h5d closed"
+     call h5fclose_F(file_id,error)
+     print*,error
+     print*,"h5f closed"
 
-1000 print*, "abort, abort, in readH5Beamfile",errorstr
+   end if
+!   CALL h5pcreate_f(H5P_FILE_ACCESS_F, plist_id, error)
+! In the VERY short term, we'll just read on rank 0
+!   CALL h5pset_fapl_mpio_f(plist_id, tProcInfo_G%comm, mpiinfo, error)
+
+! Allocate local MP arrays
+    nMPsLoc=nMPs
+
+
+    allocate(sElX_G(nMPsLoc),   &
+             sElY_G(nMPsLoc),   &
+             sElZ2_G(nMPsLoc),  &
+             sElPX_G(nMPsLoc),  &
+             sElPY_G(nMPsLoc),  &
+             sElGam_G(nMPsLoc), &
+             s_chi_bar_G(nMPsLoc), &
+             s_Normalised_chi_G(nMPsLoc))
+! open up the file and read
+      if (tProcInfo_G%qRoot) then 
+      CALL h5fopen_f(filename, H5F_ACC_RDONLY_F, file_id, error)
+     print*,error
+     print*,"h5f open"
+      CALL h5dopen_f (file_id, dsetname, dset_id, error)
+     print*,error
+     print*,"h5d open"
+      dims=(/1,nMPs/)
+      dsize=(/7,nMPs/)
+      doffset=(/0,0/)
+      CALL h5screate_simple_f(rank, dims, memspace, error)
+     print*,error
+     print*,"h5s mem created"
+!      CALL h5screate_simple_f(rank, dsize, filespace, error)
+!      CALL h5screate_simple_f(rank, dsize, dspace_id, error)
+     print*,error
+     print*,"h5s file created"
+      CALL h5Dget_space_f(dset_id,dspace_id,error)
+     print*,error
+     print*,"h5d space got"
+
+      CALL h5sselect_hyperslab_f(dspace_id, H5S_SELECT_SET_F, doffset, &
+       dims, error)
+!      CALL h5sselect_hyperslab_f(filespace, H5S_SELECT_SET_F, doffset, &
+!       dims, error)
+     print*,error
+     print*,"h5s slab selected"
+      CALL h5dread_f(dset_id, H5T_NATIVE_DOUBLE, sElX_G, dims, error, &
+       file_space_id = dspace_id, mem_space_id = memspace)
+     print*,error
+     print*,"h5d slab read"
+!      call h5sclose_f(dspace_id,error)
+!     print*,error
+!     print*,"h5s dspace closed"
+
+      doffset=(/1,0/)
+      CALL h5sselect_hyperslab_f(dspace_id, H5S_SELECT_SET_F, doffset, &
+       dims, error)
+     print*,error
+     print*,"h5s slab1 selected"
+      CALL h5dread_f(dset_id, H5T_NATIVE_DOUBLE, sElY_G, dims, error, &
+       file_space_id = dspace_id, mem_space_id = memspace)
+     print*,error
+     print*,"h5d slab1 read"
+!      call h5sclose_f(dspace_id,error)
+
+      doffset=(/2,0/)
+      CALL h5sselect_hyperslab_f(dspace_id, H5S_SELECT_SET_F, doffset, &
+       dims, error)
+     print*,"h5s slab2 selected"
+      CALL h5dread_f(dset_id, H5T_NATIVE_DOUBLE, sElZ2_G, dims, error, &
+       file_space_id = dspace_id, mem_space_id = memspace)
+     print*,error
+     print*,"h5d slab2 read"
+!      call h5sclose_f(dspace_id,error)
+
+      doffset=(/3,0/)
+      CALL h5sselect_hyperslab_f(dspace_id, H5S_SELECT_SET_F, doffset, &
+       dims, error)
+      CALL h5dread_f(dset_id, H5T_NATIVE_DOUBLE, sElPX_G, dims, error, &
+       file_space_id = dspace_id, mem_space_id = memspace)
+     print*,error
+     print*,"h5d slab3 read"
+!      call h5sclose_f(dspace_id,error)
+
+      doffset=(/4,0/)
+      CALL h5sselect_hyperslab_f(dspace_id, H5S_SELECT_SET_F, doffset, &
+       dims, error)
+      CALL h5dread_f(dset_id, H5T_NATIVE_DOUBLE, sElPY_G, dims, error, &
+       file_space_id = dspace_id, mem_space_id = memspace)
+     print*,error
+     print*,"h5d slab4 read"
+!      call h5sclose_f(dspace_id,error)
+
+      doffset=(/5,0/)
+      CALL h5sselect_hyperslab_f(dspace_id, H5S_SELECT_SET_F, doffset, &
+       dims, error)
+      CALL h5dread_f(dset_id, H5T_NATIVE_DOUBLE, sElGam_G, dims, error, &
+       file_space_id = dspace_id, mem_space_id = memspace)
+     print*,error
+     print*,"h5d slab5 read"
+!      call h5sclose_f(dspace_id,error)
+
+      doffset=(/6,0/)
+      CALL h5sselect_hyperslab_f(dspace_id, H5S_SELECT_SET_F, doffset, &
+       dims, error)
+      CALL h5dread_f(dset_id, H5T_NATIVE_DOUBLE, s_chi_bar_G, dims, error, &
+       file_space_id = dspace_id, mem_space_id = memspace)
+     print*,error
+     print*,"h5d slab6 read"
+      call h5sclose_f(dspace_id,error)
+     print*,"h5s dspace_id closed"
+
+
+      call h5sclose_f(memspace,error)
+     print*,"h5s memspace closed"
+!      call h5sclose_f(filespace,error)
+      call h5dclose_f(dset_id,error)
+     print*,"h5d dset closed"
+      call h5fclose_f(file_id,error)
+     print*,"h5f file_id closed"
+      end if !not on root any more.
+
+!
+! Low quality smoke test
+!
+print*,"Low grade smoke test"
+print*,SElx_G(100)
+print*,S_chi_bar_G(200)
+print*,"Now, you tell me if we had electrons"
+    GoTo 2000
+
+! Error Handler - Error log Subroutine in CIO.f90 line 709
+1000 call Error_log('Error in H5in:readH5BeamFile',&
+          tErrorLog_G)
+   print*, "abort, abort, Error in readH5Beamfile",errorstr
+2000 CONTINUE
   end subroutine readH5Beamfile
 
 end module H5in
