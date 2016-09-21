97c64d8e4b51e649f45078bd47c1b28c7347a493
Lawrence Campbell <lawrence.campbell@strath.ac.uk>
2016-09-21 18:55:41 +0100

    TEMP COMMIT - WILL NOT COMPILE
    Beginning to mess around with data layout...
    
    -Lawrence

--- source/MRK4.f90
+++ source/MRK4.f90
@@ -35,7 +35,9 @@ implicit none
   REAL(KIND=WP), DIMENSION(:), ALLOCATABLE :: dpxm, dpxt, pxt
   REAL(KIND=WP), DIMENSION(:), ALLOCATABLE :: dpym, dpyt, pyt
   REAL(KIND=WP), DIMENSION(:), ALLOCATABLE :: dz2m, dz2t, z2t
-  REAL(KIND=WP), DIMENSION(:), ALLOCATABLE :: dpz2m, dpz2t, pz2t  
+  REAL(KIND=WP), DIMENSION(:), ALLOCATABLE :: dpz2m, dpz2t, pz2t
+
+  real(kind=wp), allocatable :: dEdx(:,:), dEm(:,:), dEt(:,:), Et(:,:)
 
 contains
 
@@ -44,10 +46,10 @@ subroutine rk4par(sZ,h,qD)
   implicit none
 !
 ! Perform 4th order Runge-Kutta integration, tailored
-! to Puffin and its method of parallelization: 
+! to Puffin and its method of parallelization:
 ! This is NOT a general, all-purpose RK4 routine, it
-! is specific to Puffin. Includes MPI_gathers and 
-! scatters etc between calculation of derivatives for 
+! is specific to Puffin. Includes MPI_gathers and
+! scatters etc between calculation of derivatives for
 ! use with the parallel field derivative.
 !
 !                ARGUMENTS
@@ -56,7 +58,7 @@ subroutine rk4par(sZ,h,qD)
 ! SA      INPUT/OUTPUT   Field values
 ! x       INPUT          Propagation distance zbar
 ! h       INPUT          Step size in zbar
-      
+
 !  REAL(KIND=WP),  DIMENSION(:), INTENT(INOUT) :: sA, A_local
   REAL(KIND=WP),  INTENT(IN)                  :: sZ
   REAL(KIND=WP),                INTENT(IN)  :: h
@@ -65,7 +67,7 @@ subroutine rk4par(sZ,h,qD)
 !               LOCAL ARGS
 !
 ! h6         Step size divided by 6
-! hh         Half of the step size 
+! hh         Half of the step size
 ! xh         x position incremented by half a step
 ! dym        Intermediate derivatives
 ! dyt        Intermediate derivatives
@@ -81,7 +83,7 @@ subroutine rk4par(sZ,h,qD)
 
 
   REAL(KIND=WP), DIMENSION(:),ALLOCATABLE :: dAdx
-  REAL(KIND=WP), DIMENSION(:),ALLOCATABLE :: A_localt 
+  REAL(KIND=WP), DIMENSION(:),ALLOCATABLE :: A_localt
   INTEGER(KIND=IP) :: error, trans
 
 !    Transverse nodes
@@ -90,7 +92,7 @@ subroutine rk4par(sZ,h,qD)
 
 !    Step sizes
 
-  hh = h * 0.5_WP    
+  hh = h * 0.5_WP
   h6 = h / 6.0_WP
   szh = sz + hh
 
@@ -153,7 +155,7 @@ subroutine rk4par(sZ,h,qD)
 !  allocate(DADx(2*local_rows))
 !  allocate(A_localt(2*local_rows))
 
-!    A_local from A_big	  
+!    A_local from A_big
 
   if (qD) then
 
@@ -172,7 +174,7 @@ subroutine rk4par(sZ,h,qD)
 !    First step
 !  iy = size(sElX_G)
 !  idydx = size(dxdx)
-  
+
 !    Get derivatives
 
   call derivs(sZ, A_localtr0, A_localti0, &
@@ -187,7 +189,7 @@ subroutine rk4par(sZ,h,qD)
   !print*, dpydx
 
 !    Increment local electron and field values
-  
+
   if (qPArrOK_G) then
 
     xt = sElX_G      +  hh*dxdx
@@ -200,7 +202,7 @@ subroutine rk4par(sZ,h,qD)
     A_localtr1 = A_localtr0 + hh * dadz_r0
     A_localti1 = A_localti0 + hh * dadz_i0
 
-!    Update large field array with new values 
+!    Update large field array with new values
 !  call local2globalA(A_localt,sA,recvs,displs,tTransInfo_G%qOneD)
 
     call upd8a(A_localtr1, A_localti1)
@@ -209,7 +211,7 @@ subroutine rk4par(sZ,h,qD)
 
 
 
-!    Second step       
+!    Second step
 !    Get derivatives
 
   if (qPArrOK_G) &
@@ -244,7 +246,7 @@ subroutine rk4par(sZ,h,qD)
 
   end if
 
-!    Third step       
+!    Third step
 !    Get derivatives
 
 
@@ -288,21 +290,21 @@ subroutine rk4par(sZ,h,qD)
 
   end if
 
-!    Fourth step       
+!    Fourth step
 
   szh = sz + h
 
 
 !    Get derivatives
 
-  if (qPArrOK_G) & 
+  if (qPArrOK_G) &
       call derivs(szh, A_localtr3, A_localti3, &
        xt, yt, z2t, pxt, pyt, pz2t, &
        dxt, dyt, dz2t, dpxt, dpyt, dpz2t, &
-       dadz_r1, dadz_i1)  
+       dadz_r1, dadz_i1)
 
 
-!    Accumulate increments with proper weights       
+!    Accumulate increments with proper weights
 
   if (qPArrOK_G) then
 
@@ -348,14 +350,14 @@ subroutine rk4par(sZ,h,qD)
 !  deallocate(A_localtr2, A_localti2)
 !  deallocate(A_localtr3, A_localti3)
 !
-!  deallocate(DxDx)    
-!  deallocate(DyDx)    
-!  deallocate(DpxDx)    
-!  deallocate(DpyDx)    
-!  deallocate(Dz2Dx)    
-!  deallocate(Dpz2Dx)    
+!  deallocate(DxDx)
+!  deallocate(DyDx)
+!  deallocate(DpxDx)
+!  deallocate(DpyDx)
+!  deallocate(Dz2Dx)
+!  deallocate(Dpz2Dx)
 
-!   Set error flag and exit         
+!   Set error flag and exit
 
   GOTO 2000
 
@@ -379,12 +381,17 @@ subroutine allact_rk4_arrs()
 
   tllen43D = tllen * ntrndsi_G
 
-  allocate(DxDx(iNumberElectrons_G))    
-  allocate(DyDx(iNumberElectrons_G))    
-  allocate(DpxDx(iNumberElectrons_G))    
-  allocate(DpyDx(iNumberElectrons_G))    
-  allocate(Dz2Dx(iNumberElectrons_G))    
-  allocate(Dpz2Dx(iNumberElectrons_G))    
+
+
+
+!  allocate(DxDx(iNumberElectrons_G))
+!  allocate(DyDx(iNumberElectrons_G))
+!  allocate(DpxDx(iNumberElectrons_G))
+!  allocate(DpyDx(iNumberElectrons_G))
+!  allocate(Dz2Dx(iNumberElectrons_G))
+!  allocate(Dpz2Dx(iNumberElectrons_G))
+
+  allocate(dEdx(7_ip, iNumberElectrons_G))
 
 
   allocate(dadz_r0(tllen43D), dadz_i0(tllen43D))
@@ -398,18 +405,23 @@ subroutine allact_rk4_arrs()
 
   allocate(ac_rfield_in(tllen43D), ac_ifield_in(tllen43D))
 
-  allocate(dxm(iNumberElectrons_G), &
-    dxt(iNumberElectrons_G), xt(iNumberElectrons_G))
-  allocate(dym(iNumberElectrons_G), &
-    dyt(iNumberElectrons_G), yt(iNumberElectrons_G))
-  allocate(dpxm(iNumberElectrons_G), &
-    dpxt(iNumberElectrons_G), pxt(iNumberElectrons_G))
-  allocate(dpym(iNumberElectrons_G), &
-    dpyt(iNumberElectrons_G), pyt(iNumberElectrons_G))
-  allocate(dz2m(iNumberElectrons_G), &
-    dz2t(iNumberElectrons_G), z2t(iNumberElectrons_G))
-  allocate(dpz2m(iNumberElectrons_G), &
-    dpz2t(iNumberElectrons_G), pz2t(iNumberElectrons_G))
+
+!  allocate(dxm(iNumberElectrons_G), &
+!    dxt(iNumberElectrons_G), xt(iNumberElectrons_G))
+!  allocate(dym(iNumberElectrons_G), &
+!    dyt(iNumberElectrons_G), yt(iNumberElectrons_G))
+!  allocate(dpxm(iNumberElectrons_G), &
+!    dpxt(iNumberElectrons_G), pxt(iNumberElectrons_G))
+!  allocate(dpym(iNumberElectrons_G), &
+!    dpyt(iNumberElectrons_G), pyt(iNumberElectrons_G))
+!  allocate(dz2m(iNumberElectrons_G), &
+!    dz2t(iNumberElectrons_G), z2t(iNumberElectrons_G))
+!  allocate(dpz2m(iNumberElectrons_G), &
+!    dpz2t(iNumberElectrons_G), pz2t(iNumberElectrons_G))
+
+  allocate(dEm(7_ip, iNumberElectrons_G), &
+           dEt(7_ip, iNumberElectrons_G), &
+           Et(7_ip, iNumberElectrons_G))
 
   allocate(dadz_w(iNumberElectrons_G))
 
@@ -437,15 +449,15 @@ subroutine deallact_rk4_arrs()
   deallocate(A_localtr2, A_localti2)
   deallocate(A_localtr3, A_localti3)
 
-  deallocate(DxDx)    
-  deallocate(DyDx)    
-  deallocate(DpxDx)    
-  deallocate(DpyDx)    
-  deallocate(Dz2Dx)    
-  deallocate(Dpz2Dx)    
+  deallocate(DxDx)
+  deallocate(DyDx)
+  deallocate(DpxDx)
+  deallocate(DpyDx)
+  deallocate(Dz2Dx)
+  deallocate(Dpz2Dx)
 
   deallocate(dxm, &
-    dxt, xt)    
+    dxt, xt)
   deallocate(dym, &
     dyt, yt)
   deallocate(dpxm, &
@@ -481,13 +493,13 @@ end subroutine deallact_rk4_arrs
 ! Scoop out preamble of rhs, defining temp vars.
 ! These can all be defined outside this routine to make
 ! it more readable
-! 
+!
 ! label consistently - *_g for main global,
 !                      *_rg for rhs global,
 !                      *_dg for diffraction global
 !
 ! Check 3D und eqns - are they general? i.e. can kx and ky be anything?
-! 
+!
 ! Lj, field4elec (real and imag) and dp2f should be global?
 !
 ! Interface for private var access??
@@ -502,7 +514,7 @@ end subroutine deallact_rk4_arrs
 ! Rename eqns to electron eqns or something...
 !
 ! Make rhs / eqns vars global
-! fix dp2f interface in rhs.f90 ... DONE 
+! fix dp2f interface in rhs.f90 ... DONE
 ! only allocate / calc dp2f when it will be used!
 !
 
