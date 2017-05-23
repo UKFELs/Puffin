9e4d21e24c2805c756957a55feb7930834043d49
Lawrence Campbell <lawrence.campbell@strath.ac.uk>
2017-05-23 13:27:48 +0100

    Updating fortran type for Puffin scaling.
    -Lawrence

--- source/typeScale.f90
+++ source/typeScale.f90
@@ -6,7 +6,7 @@
 
 !> @author
 !> Lawrence Campbell,
-!> University of Strathclyde, 
+!> University of Strathclyde,
 !> Glasgow, UK
 !> @brief
 !> This module contains the type definition to hold the scaling constants
@@ -15,17 +15,21 @@
 module typeScale
 
   use paratype
-		
+
   implicit none
-      
+
   type fScale
-  
+
     real(kind=wp) :: eta
     real(kind=wp) :: rho
     real(kind=wp) :: aw
     real(kind=wp) :: lambda_w
     real(kind=wp) :: gamma_r
-    
+
+    real(kind=wp) :: lambda_r
+    real(kind=wp) :: lc, lg
+    real(kind=wp) :: ux, uy
+
   end type fScale
-  
+
 end module typeScale
