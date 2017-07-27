1ee3c8a007da4b50f9e53990bfc65aa53c557294
Lawrence Campbell <lawrence.campbell@strath.ac.uk>
2017-07-28 00:05:02 +0100

    ReorderColMajor... now operates on the inputfile basename, much like powPrep, for convenience.
    -Lawrence

--- utilities/post/ReorderColMajorFtoColMinorC.py
+++ utilities/post/ReorderColMajorFtoColMinorC.py
@@ -7,7 +7,27 @@
 
 A script for conversion of VizSchema in CompMajorF (Column Major) to CompMajorC (Column Minor)
 """
-import numpy,tables,sys
+
+import numpy, tables, sys, os, glob
+
+
+
+def getFiles(baseName):
+  """ getTimeSlices(baseName) gets a list of files
+
+  That will be used down the line...
+  """
+  filelist=glob.glob(os.getcwd()+os.sep+baseName+'_aperp_*.h5')
+  
+  dumpStepNos=[]
+  for thisFile in filelist:
+    thisDump=int(thisFile.split(os.sep)[-1].split('.')[0].split('_')[-1])
+    dumpStepNos.append(thisDump)
+
+  for i in range(len(dumpStepNos)):
+    filelist[i]=baseName+'_aperp_'+str(sorted(dumpStepNos)[i])+'.h5'
+  return filelist
+
 
 def isvsh5suffix(filename):
   return filename[-5:]==".vsh5"
@@ -19,73 +39,88 @@ if len(sys.argv)!=2:
   print "Usage ReorderColMajorFtoColMinorC.py filename"
   print "Please provide full filename, or a basename without extension to match multiple files"
   exit()
-filename = sys.argv[1]
+
+baseName = sys.argv[1]
+filelist = getFiles(baseName)
 
 
-# Need to handle a both cases where filename includes a _dumpstep.h5 and where it is a plain filename
-# if there are multiple matches,
-print "Reorder rows and columns attempting to match against: "+filename
+def convertFile(filename):
+  # Need to handle a both cases where filename includes a _dumpstep.h5 and where it is a plain filename
+  # if there are multiple matches,
+  print "Reorder rows and columns attempting to match against: "+filename
 
-if isvsh5suffix(filename):
-  filenamepieces=filename.split('_')
-  print len(filenamepieces)
-  if len(filenamepieces)==4:
-    (simbase,dataset,rank,dumpnoAndExt)=filenamepieces
-  elif len(filenamepieces)==3:
-    (simbase,dataset,dumpnoAndExt)=filenamepieces
-#  outfilename = filename[:-5]+"C.vsh5"
+  if isvsh5suffix(filename):
+    filenamepieces=filename.split('_')
+    print len(filenamepieces)
+    
+    if len(filenamepieces)==4:
+      (simbase,dataset,rank,dumpnoAndExt)=filenamepieces
+
+    elif len(filenamepieces)==3:
+      (simbase,dataset,dumpnoAndExt)=filenamepieces
+    #  outfilename = filename[:-5]+"C.vsh5"
+
+    else:
+      print "unknown file format - don't know how to tell dumps, rank, dataset from filename"
+      exit()
+
+    tdsetname=dataset+"N"
+    outfilename = simbase+"_"+dataset+"_C_"+dumpnoAndExt
+
+  elif ish5suffix(filename):
+    filenamepieces=filename.split('_')
+    print len(filenamepieces)
+    if len(filenamepieces)==4:
+      (simbase,dataset,rank,dumpnoAndExt)=filenamepieces
+    elif len(filenamepieces)==3:
+      (simbase,dataset,dumpnoAndExt)=filenamepieces
+  #  outfilename = filename[:-5]+"C.vsh5"
+    else:
+      print "unknown file format - don't know how to tell dumps, rank, dataset from filename"
+      exit()
+    tdsetname=dataset+"N"
+    outfilename = simbase+"_"+dataset+"_C_"+dumpnoAndExt
   else:
-    print "unknown file format - don't know how to tell dumps, rank, dataset from filename"
-    exit()
-  tdsetname=dataset+"N"
-  outfilename = simbase+"_"+dataset+"_C_"+dumpnoAndExt
+    print"need to figure out if we're here, as there will be multiple output files"
+  print outfilename
 
-elif ish5suffix(filename):
-  filenamepieces=filename.split('_')
-  print len(filenamepieces)
-  if len(filenamepieces)==4:
-    (simbase,dataset,rank,dumpnoAndExt)=filenamepieces
-  elif len(filenamepieces)==3:
-    (simbase,dataset,dumpnoAndExt)=filenamepieces
-#  outfilename = filename[:-5]+"C.vsh5"
+  tables.copy_file(filename,outfilename,overwrite=1)
+  h5=tables.open_file(outfilename,'r+')
+  print h5.root.aperp.shape
+  # While this works it may be better to just read using numpy.asfortranarray() 
+  # ie fieldin=numpy.asfortranarray(h5.root.aperp.read()) or whatever actually works
+  fieldin=h5.root.aperp.read()
+  print "The input has "+str(len(fieldin.shape))+" dimensions"
+  print "fieldin shape "+str(fieldin.shape)
+  testfield=numpy.asarray(fieldin, order='C')
+  print "testfield shape"+str(testfield.shape)
+  testfield=numpy.asfortranarray(fieldin)
+  print "testfield shape"+str(testfield.shape)
+  
+  if len(fieldin.shape)==4:
+    fieldout=numpy.zeros((fieldin.shape[3],fieldin.shape[2],fieldin.shape[1],fieldin.shape[0]))
+    print "fieldout shape"+str(fieldout.shape)
+    print "3d plus component"
+
+    for i in range(fieldin.shape[0]):
+      for j in range(fieldin.shape[2]):
+        fieldout[:,j,:,i]=fieldin[i,:,j,:].T
+
+  elif len(fieldin.shape)==2:
+        fieldout=fieldin.T
   else:
-    print "unknown file format - don't know how to tell dumps, rank, dataset from filename"
-    exit()
-  tdsetname=dataset+"N"
-  outfilename = simbase+"_"+dataset+"_C_"+dumpnoAndExt
-else:
-  print"need to figure out if we're here, as there will be multiple output files"
-print outfilename
+    print "Do not recognise structure of file "+filename+" setting the _C file as just a copy"
 
-tables.copy_file(filename,outfilename,overwrite=1)
-h5=tables.open_file(outfilename,'r+')
-print h5.root.aperp.shape
-# While this works it may be better to just read using numpy.asfortranarray() 
-# ie fieldin=numpy.asfortranarray(h5.root.aperp.read()) or whatever actually works
-fieldin=h5.root.aperp.read()
-print "The input has "+str(len(fieldin.shape))+" dimensions"
-print "fieldin shape "+str(fieldin.shape)
-testfield=numpy.asarray(fieldin, order='C')
-print "testfield shape"+str(testfield.shape)
-testfield=numpy.asfortranarray(fieldin)
-print "testfield shape"+str(testfield.shape)
-if len(fieldin.shape)==4:
-  fieldout=numpy.zeros((fieldin.shape[3],fieldin.shape[2],fieldin.shape[1],fieldin.shape[0]))
-  print "fieldout shape"+str(fieldout.shape)
-  print "3d plus component"
-  for i in range(fieldin.shape[0]):
-    for j in range(fieldin.shape[2]):
-      fieldout[:,j,:,i]=fieldin[i,:,j,:].T
-elif len(fieldin.shape)==2:
-      fieldout=fieldin.T
-else:
-  print "Do not recognise structure of file "+filename+" setting the _C file as just a copy"
-      #.reshape(numpy.array((h5.root.aperp.shape[3],h5.root.aperp.shape[2],h5.root.aperp.shape[1],2)),order='F')
-print fieldout.shape
-h5.create_array('/','aperpN',fieldout)
-h5.copy_node_attrs('/aperp','/aperpN')
-h5.remove_node('/aperp')
-h5.rename_node('/aperpN','aperp')
-h5.root._v_children['aperp']._v_attrs.vsIndexOrder="compMinorC"
-h5.root._v_children['meshScaled']._v_attrs.vsIndexOrder="compMinorC"
-h5.close()
+  print fieldout.shape
+
+  h5.create_array('/','aperpN',fieldout)
+  h5.copy_node_attrs('/aperp','/aperpN')
+  h5.remove_node('/aperp')
+  h5.rename_node('/aperpN','aperp')
+  h5.root._v_children['aperp']._v_attrs.vsIndexOrder="compMinorC"
+  h5.root._v_children['meshScaled']._v_attrs.vsIndexOrder="compMinorC"
+  h5.close()
+
+
+for iname in filelist:
+  convertFile(iname)
