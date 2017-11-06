22e44dd226fb2b14ecefa89bc19eaf3e2bc5296a
Lawrence Campbell <lawrence.campbell@strath.ac.uk>
2017-11-06 15:16:51 +0000

    Updating Python gen scripts to new format - now complies with new Puffin input files (at last!).
    -Lawrence

--- inputs/gen/beamClass.py
+++ inputs/gen/beamClass.py
@@ -3,83 +3,103 @@
 # License: BSD-3-Clause
 
 import math
+import numpy as np
 import fBoolean
 
 class bm:
     """This class defines the electron beam beam data."""
-    sigx  = 0.1
-    sigy  = 0.1
-    sigz2 = 1E8
-    sigpx = 0.5
-    sigpy = 0.5
-    sigp2 = 1E8
-    lex = 0.6
-    ley = 0.6
-    lez2 = 1
-    lepx = 3
-    lepy = 3
-    lep2 = 0.001
-    nmpx  = 1
-    nmpy  = 1
-    nmpz2 = 5
-    nmppx = 3
-    nmppy = 3
-    nmpp2 = 1
-    qmatch = 1
-    eratio = 1
-    emit_bar = 1
-    chirp = 0
-    bcenz2 = 0
-    Q = 1e-12
-    qRoundEj = 1
-    sigEj = 1
-    bosc_Mag = 0
-    bosc_Fr = 1
+    nbeams = 1
+    bftype = 'simple'
+    sig = np.array([1., 1., 1., 1., 1., 1.])
+    le = np.array([6., 6., 6., 6., 6., 6.])
+    nmps  = np.ones(1) * 1
+    qmatch = np.ones(1) * 1
+    eratio = np.ones(1) * 1
+    emitx = np.ones(1) * 1
+    emity = np.ones(1) * 1
+    alphax = np.ones(1) * 0
+    alphay = np.ones(1) * 0
+    chirp = np.ones(1) * 0
+    bcenz2 = np.ones(1) * 0
+    Q = np.ones(1) * 1e-12
+    qRoundEj = np.ones(1) * 1
+    sigEj = np.ones(1) * 1
+    bosc_Mag = np.ones(1) * 0
+    bosc_Fr = np.ones(1) * 1
+    qmatch = [False]
+    trload = 2
+    qequixy = [False]
     
     
     def wrbeam(self,f,no):
+        
         torf = fBoolean.torf
-        f.write('========================================================================\n')
-        f.write('BEAM ' + '{:d}'.format(int(no)) + ':-\n')
+
+        f.write('! PUFFIN BEAM FILE\n')
+        f.write('!\n')
+        f.write('! Describes electron beams for input into puffin. Multiple beams with\n')
+        f.write('! different parameters can be used. Please refer to POP-REF for an\n')
+        f.write('! explanation of the scaled variables used, such as z2 and p2.\n')
+        f.write('!\n')
+        f.write('! namelist NBLIST - Number and type of electron beams\n')
+        f.write('!\n')
+        f.write('! nbeams - number of electron beams\n')
+        f.write('! dtype  - Input type - simple, distribution, or macroparticle \n')
+        f.write('!\n')
+        f.write('! namelist BLIST - SIMPLE BEAM PARAMETERS\n')
+        f.write('!========================================================================\n')
+        f.write('! sSigmaE - gaussian std dev in each dimension - x, y, z2, px, py, gamma, then for additional beams\n')
+        f.write('! sLenE   - Total length of beam modelled in each dimension - x, y, z2, px, py, gamma...\n')
+        f.write('! bcenter - Center of beam in z2\n')
+        f.write('! iNumElectrons  -  Number of macroparticles in each dimension used to model the beam \n')
+        f.write('! sEmit_n - Scaled transverse beam emittance\n')
+        f.write('! sQe - Beam charge \n')
+        f.write('! gammaf - Ratio of average beam energy to reference beam energy gamma / gamma_r \n')
+        f.write('! chirp  - Energy chirp in z2 i.e. dgamma/dz2\n')
+        f.write('! mag    - magnitude of energy oscillation on beam\n')
+        f.write('! fr     - frequency in z2 of beam energy oscillation\n')
+        f.write('! qRndEj_G - Round edge of flat top?\n')
+        f.write('! sSigEj_G - Gaussian sigma of tail-off if used\n')
+        f.write('! qMatched_A - Automatically match beam to focusing channel??\n')
+        f.write('! TrLdMeth - Beam loading method in 5D transverse and energy planes:\n')
+        f.write('!                TrLdMeth = 1 for random sequences (default)\n')
+        f.write('!                TrLdMeth = 2 for Halton sequences\n')
+        f.write('!========================================================================\n')
+
         f.write('\n')
-        f.write('STANDARD DEVIATION OF BEAM IN EACH DIMENSION (=1E8 FOR FLAT TOP)   - REAL\n')
         f.write('\n')
-        f.write('{:<24.15E}'.format(self.sigx) + 'SIGX       (overwritten for matched beam)\n')
-        f.write('{:<24.15E}'.format(self.sigy) + 'SIGY       (overwritten for matched beam)\n')
-        f.write('{:<24.15E}'.format(self.sigz2) + 'SIGZ2\n')
-        f.write('{:<24.15E}'.format(self.sigpx) + 'SIGPX      (overwritten for matched beam)\n')
-        f.write('{:<24.15E}'.format(self.sigpy) + 'SIGPY      (overwritten for matched beam)\n')
-        f.write('{:<24.15E}'.format(self.sigp2) + 'SIGGAMMA\n')
         f.write('\n')
-        f.write('TOTAL MODELLED LENGTH OF BEAM IN EACH DIMENSION    - REAL\n')
+
+        f.write('&NBLIST\n')
+        f.write('nbeams = ' + '{:d}'.format(self.nbeams) + '\n')
+        f.write('dtype = ' + '\'' + self.bftype + '\'' + '\n')
+        f.write('/\n')
+
         f.write('\n')
-        f.write('{:<24.15E}'.format(self.lex)  + 'LENX       (overwritten for matched beam)\n')
-        f.write('{:<24.15E}'.format(self.ley)  + 'LENY       (overwritten for matched beam)\n')
-        f.write('{:<24.15E}'.format(self.lez2)  + 'LENZ2\n')
-        f.write('{:<24.15E}'.format(self.lepx)  + 'LENPX      (overwritten for matched beam)\n')
-        f.write('{:<24.15E}'.format(self.lepy)  + 'LENPY      (overwritten for matched beam)\n')
-        f.write('{:<24.15E}'.format(self.lep2)  + 'LENGAMMA\n')
         f.write('\n')
-        f.write('NUMBER OF MACROPARTICLES TO MODEL BEAM IN EACH DIMENSION (EQUISPACED BEFORE NOISE IS ADDED, IF USED)   - INTEGER\n')
         f.write('\n')
-        f.write('{:<24d}'.format(int(math.ceil(self.nmpx)))    + 'NUMX\n')
-        f.write('{:<24d}'.format(int(math.ceil(self.nmpy)))    + 'NUMY\n')
-        f.write('{:<24d}'.format(int(math.ceil(self.nmpz2)))   + 'NUMZ2\n')
-        f.write('{:<24d}'.format(int(math.ceil(self.nmppx)))   + 'NUMPX\n')
-        f.write('{:<24d}'.format(int(math.ceil(self.nmppy)))   + 'NUMPY\n')
-        f.write('{:<24d}'.format(int(math.ceil(self.nmpp2)))   + 'NUMGAMMA\n')
-        f.write('\n')
-        f.write('BEAM PARAMETERS\n')
-        f.write('\n')
-        f.write('{:<24}'.format(torf(self.qmatch)) + 'Match beam to undulator?\n')
-        f.write('{:<24.15E}'.format(self.eratio)  + 'Ratio of average beam energy to resonant beam energy gamma / gamma_r\n')
-        f.write('{:<24.15E}'.format(self.emit_bar)  + 'Scaled emittance (bar{epsilon})\n')
-        f.write('{:<24.15E}'.format(self.chirp) + 'Energy chirp in z2 i.e. dgamma/dz2\n')
-        f.write('{:<24.15E}'.format(self.bcenz2) + 'Center of beam in z2\n')
-        f.write('{:<24.15E}'.format(self.bosc_Mag) + 'Magnitude of oscillation of gamma in z2\n')
-        f.write('{:<24.15E}'.format(self.bosc_Fr) + 'Frequency of oscillation of gamma in z2 (kz2 = 2pi / lamda_z2)\n')
-        f.write('{:<24}'.format(torf(self.qRoundEj)) + 'Round edge if flat top?\n')
-        f.write('{:<24.15E}'.format(self.sigEj) + 'Sigma or rounded edge in z2 (if round edged flat top)\n')
-        f.write('{:<24.15E}'.format(self.Q) + 'Charge in coulombs\n')
-        f.write('========================================================================\n')
-        f.write('\n')
+
+        f.write('&BLIST\n')
+        f.write('sSigmaE = ' + ', '.join(['{:.15E}'.format(ij) for ij in self.sig])  +  '\n')
+        f.write('sLenE = ' + ', '.join(['{:.15E}'.format(ij) for ij in self.le])  +  '\n')
+        f.write('iNumElectrons = ' + ', '.join(['{:d}'.format(ij) for ij in self.nmps])  +  '\n')
+        f.write('sQe = ' + ', '.join(['{:.15E}'.format(ij) for ij in self.Q])  + '\n')
+        f.write('bcenter = ' + ', '.join(['{:.15E}'.format(ij) for ij in self.bcenz2]) + '\n')
+
+        f.write('gammaf = ' + ', '.join(['{:.15E}'.format(ij) for ij in self.eratio]) + '\n')
+        f.write('chirp = ' + ', '.join(['{:.15E}'.format(ij) for ij in self.chirp]) + '\n')
+        f.write('mag = ' + ', '.join(['{:.15E}'.format(ij) for ij in self.bosc_Mag]) + '\n')
+        f.write('fr = ' + ', '.join(['{:.15E}'.format(ij) for ij in self.bosc_Fr]) + '\n')
+        f.write('emitx = ' + ', '.join(['{:.15E}'.format(ij) for ij in self.emitx]) + '\n')
+        f.write('emity = ' + ', '.join(['{:.15E}'.format(ij) for ij in self.emity]) + '\n')
+        f.write('alphax = ' + ', '.join(['{:.15E}'.format(ij) for ij in self.alphax]) + '\n')
+        f.write('alphay = ' + ', '.join(['{:.15E}'.format(ij) for ij in self.alphay]) + '\n')
+        f.write('qRndEj_G = ' + ', '.join([torf(ij) for ij in self.qRoundEj]) + '\n')
+        f.write('sSigEj_G = ' + ', '.join(['{:.15E}'.format(ij) for ij in self.sigEj]) + '\n')
+        f.write('qMatched_A = ' + ', '.join([torf(ij) for ij in self.qmatch]) + '\n')
+        f.write('TrLdMeth = ' + ', '.join(['{:d}'.format(ij) for ij in self.trload]) + '\n')
+        f.write('/\n')
+
+
+
+
